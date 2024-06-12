35e02c6720fc1c8a3902b401758c8df0944be1f4
rds <rasul@futureblur.com>
2024-06-12 21:37:14 +0200

    perf: add astro critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://news.futureblur.com",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [tailwind(), sitemap(), mdx(), pagefind(), critters()],
   markdown: {
     shikiConfig: {
-      theme: "css-variables",
-    },
-  },
+      theme: "css-variables"
+    }
+  }
 });
