7a1f511b2b9a37cad4b8a3908c8630930d802169
rds <rasul@futureblur.com>
2024-06-12 20:15:37 +0200

    Initial commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,18 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-  }),
+	type: "content",
+	schema: ({image}) => z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+		type: z.string(),
+		cover: image().refine((img) => img.width >= 1080, {
+			message: "Cover image must be at least 1080px wide",
+		}),
+		url: z.string(),
+	}),
 });
 
-const projects = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-    demoURL: z.string().optional(),
-    repoURL: z.string().optional(),
-  }),
-});
-
-export const collections = { blog, projects };
+export const collections = {blog};
